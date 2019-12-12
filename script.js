e92ea53bfd9f757449e7e9eb794336039feaae86
valentina <valentinagiannone@gmail.com>
2019-12-12 12:01:16 +0100

    step 1

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function getData() {
+ $.ajax({
+   url: "server.php",
+   method: "GET",
+   success: function(data) {
+       printChartJs(data);
+       console.log("data", data);
+   },
+   error: function(error) {
+     console.log("error", error);
+   }
+ });
+}
+
+function printChartJs(data) {
+ var ctx = document.getElementById("myChart").getContext("2d");
+ new Chart(ctx, {
+  type:"line",
+  data:{
+   label:moment.months(),
+   data:data
+  }
+ });
+}
+
+function init() {
+  getData();
+}
+$(document).ready(init);
